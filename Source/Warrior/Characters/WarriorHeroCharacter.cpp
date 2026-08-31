f7edf5814be094534587e12de0c3b62186043687
Jacob Xu <2360371212xpx@gmail.com>
2026-08-31 16:47:07 +0800

    initial project

---
+++ Source/Warrior/Characters/WarriorHeroCharacter.cpp
@@ -0,0 +1,97 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WarriorHeroCharacter.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Datasets/Input/DataAsset_InputConfig.h"
+#include "EnhancedInputSubsystems.h"
+#include "Components/Input/WarriorInputComponent.h"
+#include "WarriorGameplayTags.h"
+
+#include "WarriorDebugHelper.h"
+
+AWarriorHeroCharacter::AWarriorHeroCharacter()
+{
+	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.f);
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+
+	bUseControllerRotationPitch = false;
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("Camera Boom"));
+	CameraBoom->SetupAttachment(GetRootComponent());
+	CameraBoom->bUsePawnControlRotation = true;
+	CameraBoom->TargetArmLength = 200.f;
+	CameraBoom->SocketOffset = FVector(0.f, 55.f, 65.f);
+
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera Component"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	FollowCamera->bUsePawnControlRotation = false;
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 500.f, 0.f);
+	GetCharacterMovement()->MaxWalkSpeed = 400.f;
+	GetCharacterMovement()->BrakingDecelerationWalking = 200.f;
+
+}
+
+void AWarriorHeroCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	Debug::Print(TEXT("Working"));
+
+}
+
+void AWarriorHeroCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	checkf(InputConfigDataAsset, TEXT("Forget to assign a valid data asset as input config"));
+
+	ULocalPlayer* LocalPlayer = GetController<APlayerController>()->GetLocalPlayer();
+	UEnhancedInputLocalPlayerSubsystem*  Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(LocalPlayer);
+	check(Subsystem);
+	
+	Subsystem->AddMappingContext(InputConfigDataAsset->DefaultMappingContext, 0);
+	
+	UWarriorInputComponent* WarriorInputComponent = CastChecked<UWarriorInputComponent>(PlayerInputComponent);
+	
+	WarriorInputComponent->BindNativeInputAction(InputConfigDataAsset, WarriorGameplayTags::InputTag_Move, ETriggerEvent::Triggered, this, &AWarriorHeroCharacter::Input_Move);
+	WarriorInputComponent->BindNativeInputAction(InputConfigDataAsset, WarriorGameplayTags::InputTag_Look, ETriggerEvent::Triggered, this, &AWarriorHeroCharacter::Input_Look);
+	
+
+}
+
+void AWarriorHeroCharacter::Input_Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2D MovementVector = InputActionValue.Get<FVector2D>();
+	const FRotator MovementRotation(0.f, Controller->GetControlRotation().Yaw, 0.f);
+
+	if (MovementVector.Y != 0.f)
+	{
+		const FVector ForwardDirection = MovementRotation.RotateVector(FVector::ForwardVector);
+		AddMovementInput(ForwardDirection, MovementVector.Y);
+	}
+	
+	if (MovementVector.X != 0.f)
+	{
+		const FVector RightDirection = MovementRotation.RotateVector(FVector::RightVector);
+		AddMovementInput(RightDirection, MovementVector.X);
+
+	}
+}
+
+void AWarriorHeroCharacter::Input_Look(const FInputActionValue& InputActionValue)
+{
+	const FVector2D LookAxisVector= InputActionValue.Get<FVector2D>();
+	if (LookAxisVector.X != 0.f)
+	{
+		AddControllerYawInput(LookAxisVector.X);
+	}
+
+	if (LookAxisVector.Y != 0.f)
+	{
+		AddControllerPitchInput(LookAxisVector.Y);
+	}
+}
---
+++ Source/Warrior/Components/Input/WarriorInputComponent.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnhancedInputComponent.h"
+#include "Datasets/Input/DataAsset_InputConfig.h"
+#include "WarriorInputComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API UWarriorInputComponent : public UEnhancedInputComponent
+{
+	GENERATED_BODY()
+	
+public:
+	template<class UserObject, typename CallbackFunc>
+	void BindNativeInputAction(const UDataAsset_InputConfig* InInputConfig, const FGameplayTag& InInputTag, ETriggerEvent TriggerEvent, UserObject* ContextObject, CallbackFunc Func);
+};
+
+template<class UserObject, typename CallbackFunc>
+inline void UWarriorInputComponent::BindNativeInputAction(const UDataAsset_InputConfig* InInputConfig, const FGameplayTag& InInputTag, ETriggerEvent TriggerEvent, UserObject* ContextObject, CallbackFunc Func)
+{
+	checkf(InInputConfig, TEXT("Input Config data asset is null"));
+	if (UInputAction* FoundAction = InInputConfig->FindNativeInputActionByTag(InInputTag)) {
+		BindAction(FoundAction, TriggerEvent, ContextObject, Func);
+	}
+}
